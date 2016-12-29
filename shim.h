7d22795f0e1775095ec9cee063a025459003a27e
s1ddok <siddok@gmail.com>
2016-12-29 03:10:28 +0300

    Add android shims

--- shim.h
+++ shim.h
@@ -9,17 +9,30 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_syswm.h>
 
+void* SDL_SysWMinfo_init_version(SDL_SysWMinfo* info) {
+    SDL_VERSION(&info->version);
+}
+
 #if defined(SDL_VIDEO_DRIVER_X11)
 
-void* SDL_SysWMinfo_init_version(SDL_SysWMinfo* info) {
-  SDL_VERSION(&info->version);
-}
 void* SDL_SysWMinfo_get_x11_display(SDL_SysWMinfo* info) {
   return (void*)info->info.x11.display;
 }
 
 void* SDL_SysWMinfo_get_x11_window(SDL_SysWMinfo* info) {
   return (void*)(uintptr_t)info->info.x11.window;
+}
+
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_ANDROID)
+
+void* SDL_SysWMinfo_get_android_surface(SDL_SysWMinfo* info) {
+    return (void*)(uintptr_t)info->info.android.surface;
+}
+
+void* SDL_SysWMinfo_get_android_window(SDL_SysWMinfo* info) {
+    return (void*)info->info.android.window;
 }
 
 #endif
